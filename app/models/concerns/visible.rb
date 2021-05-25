b997d69c1c34644d70b93d6a3ce40d4e00b81967
IslamAhmed0 <ea43533@gmail.com>
2021-05-25 02:34:52 +0200

    lab one and two

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+module Visible
+  extend ActiveSupport::Concern
 
-</body>
-</html>
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  included do
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
+
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
+  def archived?
+    status == 'archived'
+  end
+end
